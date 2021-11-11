9fd6c1ba63a446a8f9cc7ee8354e015420bc967b
Yura1Chuyko <ura5509@gmail.com>
2021-11-11 22:31:33 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Calculator {
+    public static <T extends Number> double sum(final T firstNumber, final T secondNumber) {
+        return firstNumber.doubleValue() + secondNumber.doubleValue();
+    }
+
+    public static <T extends Number> double diff(final T firstNumber, final T secondNumber) {
+        return firstNumber.doubleValue() - secondNumber.doubleValue();
+    }
+
+    public static <T extends Number> double mltp(final T firstNumber, final T secondNumber) {
+        return firstNumber.doubleValue() * secondNumber.doubleValue();
+    }
+
+    public static <T extends Number> double devide(final T firstNumber, final T secondNumber) {
+        return firstNumber.doubleValue() / secondNumber.doubleValue();
+    }
+
+    public static double mltp(final CustomDouble firstNumber, final CustomDouble secondNumber) {
+        return firstNumber.toDouble() * secondNumber.toDouble();
+    }
+
+    public static double devide(final CustomDouble firstNumber, final CustomDouble secondNumber) {
+        return firstNumber.toDouble() / secondNumber.toDouble();
+    }
+
+    public static void main(final String[] args) {
+        final CustomDouble a = new CustomDouble(1, 0.5);
+        final CustomDouble b = new CustomDouble(2, 0.3);
+        a.plus(b);
+        System.out.println(devide(a, b));
+        System.out.println(mltp(a, b));
+        System.out.println(sum(1.2, 1.2));
+    }
+}
+
